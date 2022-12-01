bdb3b3954b2c6a191139ec7f1bffe74ec9a13646
Veronica Lusby <vlusby@indeed.com>
2022-12-01 11:22:38 -0600

    [AOC-2022] Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,62 @@
+import java.util.Comparator
+import java.util.PriorityQueue
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun findMaxCalories1(input: List<String>): Long {
+        var maxCalories = 0L
+        var currentCaloriesPerElf = 0L
+        for(item in input) {
+            if (item.isEmpty()) {
+                if (currentCaloriesPerElf > maxCalories) {
+                    maxCalories  = currentCaloriesPerElf
+                }
+                currentCaloriesPerElf = 0L
+            } else {
+                var calorie = item.toLong()
+                currentCaloriesPerElf += calorie
+            }
+
+        }
+        return maxCalories
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun findMaxCalories2(input: List<String>): Long {
+        val myCustomComparator =  Comparator<Long> { a, b ->
+            when {
+                (a == b) -> 0
+                (a > b) -> -1
+                else -> 1
+            }
+        }
+        val maxHeap = PriorityQueue<Long>(myCustomComparator)
+        var currentCaloriesPerElf = 0L
+        for(item in input) {
+            if (item.isEmpty()) {
+                maxHeap.add(currentCaloriesPerElf)
+                currentCaloriesPerElf = 0L
+            } else {
+                var calorie = item.toLong()
+                currentCaloriesPerElf += calorie
+            }
+        }
+
+        maxHeap.add(currentCaloriesPerElf)
+
+        val top1 = maxHeap.remove()
+        val top2 = maxHeap.remove()
+        val top3 = maxHeap.remove()
+
+        return top1 + top2 + top3
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(findMaxCalories1(testInput))
+    check(findMaxCalories1(testInput) == 24000L)
+
+    println(findMaxCalories2(testInput))
+    check(findMaxCalories2(testInput) == 45000L)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(findMaxCalories2(input))
 }
