4f5584a3f12a2da95c88877caae09ee6e1306f42
Veronica Lusby <vlusby@indeed.com>
2022-12-03 12:12:21 -0600

    [AOC-2022] Day3

--- src/Utils.kt
+++ src/Utils.kt
@@ -15,24 +15,22 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
     .toString(16)
     .padStart(32, '0')
 
-///** this will make day 1 solution a lot cleaner
-// * stole the idea from https://github.com/ylegall/AOC-Kotlin
-// */
-//fun splitToListOfList(input: List<String>):List<List<String>> {
-//    val block = mutableListOf<List<String>>()
-//    val currentBlock = mutableListOf<String>()
-//    for (item in input) {
-//        if (item.isEmpty()) {
-//            if (currentBlock.isNotEmpty()) {
-//                block.add(currentBlock)
-//                currentBlock.clear()
-//            }
-//        } else {
-//            currentBlock.add(item)
-//        }
-//    }
-//    if (currentBlock.isNotEmpty()) {
-//        block.add(currentBlock)
-//    }
-//    return block
-//}
+/**
+ * stole the idea from https://github.com/ylegall/AOC-Kotlin
+ */
+fun splitToListOfList(input: List<String>): List<List<String>> {
+    val block = mutableListOf<List<String>>()
+    val currentBlock = mutableListOf<String>()
+    for ((counter, item) in input.withIndex()) {
+        if (counter % 3 == 2 ) {
+            if (currentBlock.isNotEmpty()) {
+                currentBlock.add(item)
+                block.add(currentBlock.toList())
+                currentBlock.clear()
+            }
+        } else {
+            currentBlock.add(item)
+        }
+    }
+    return block.toList()
+}
---
+++ src/day03/Day03.kt
@@ -0,0 +1,50 @@
+package day03
+import readInput
+import splitToListOfList
+
+fun main() {
+    fun findItemsScore(input: String): Int {
+        val firstBag = input.substring(0, input.length/2).toSet()
+        val secondBag = input.substring(input.length/2).toSet()
+        return firstBag.intersect(secondBag).sumOf { item ->
+            when (item.isUpperCase()) {
+                true -> item - 'A' + 27
+                false -> item - 'a' + 1
+            }
+        }
+    }
+    fun findBadgeScore(input: List<String>): Int {
+        val firstElf = input[0].toSet()
+        val secondElf = input[1].toSet()
+        val thirdElf = input[2].toSet()
+        return firstElf.intersect(secondElf).intersect(thirdElf).sumOf { item ->
+            when (item.isUpperCase()) {
+                true -> item - 'A' + 27
+                false -> item - 'a' + 1
+            }
+        }
+    }
+    fun findBackpackItem1(input: List<String>): Int {
+        return input.sumOf { item ->
+            findItemsScore(item)
+        }
+    }
+
+    fun findBackpackItem2(input: List<String>): Int {
+        return splitToListOfList(input).sumOf { item ->
+            findBadgeScore(item)
+        }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("day03/Day03_test")
+    println(findBackpackItem1(testInput))
+    check(findBackpackItem1(testInput) == 157)
+
+    println(findBackpackItem2(testInput))
+    check(findBackpackItem2(testInput) == 70)
+
+    val input = readInput("day03/Day03")
+    println(findBackpackItem1(input))
+    println(findBackpackItem2(input))
+}
